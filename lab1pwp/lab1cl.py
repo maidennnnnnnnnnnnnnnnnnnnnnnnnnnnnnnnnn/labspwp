b93809948f817fca165fb407b123a48b20524058
maidennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn <srazihely@gmail.com>
2023-10-11 18:15:06 +0300

    task3 added multiple message support

--- lab1pwp/lab1cl.py
+++ lab1pwp/lab1cl.py
@@ -1,15 +1,19 @@
 import socket
 
 host = '127.0.0.1'
-port = 12345
+port = 52345
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((host, port))
+while True:
+    message = input("Введіть текст: ")
 
-message = input("Введіть текст: ")
+    client_socket.sendall(message.encode('utf-8'))
+    client_socket.sendall(str(len(message)).encode('utf-8'))
+    if message == "stop":
+        break
+    data = client_socket.recv(1024).decode('utf-8')
+    print(data)
 
-client_socket.sendall(message.encode('utf-8'))
-client_socket.sendall(str(len(message)).encode('utf-8'))
-data = client_socket.recv(1024)
-print(data)
+
 client_socket.close()
--- lab1pwp/lab1server.py
+++ lab1pwp/lab1server.py
@@ -2,7 +2,7 @@ import socket
 import time
 
 host = '127.0.0.1'
-port = 12345
+port = 52345
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((host, port))
@@ -13,18 +13,23 @@ print(f"Сервер: {host}:{port}")
 client_socket, client_address = server_socket.accept()
 print(f"{client_address} приєднався")
 
-data = client_socket.recv(1024)
-size = int(client_socket.recv(1024).decode('utf-8'))
-time.sleep(5)
+while True:
+    data = client_socket.recv(1024).decode('utf-8')
+    size = int(client_socket.recv(1024).decode('utf-8'))
+    if not data:
+        break
+    if data == "stop":
+        break
+    # time.sleep(5)
 
-if size == len(data):
+    if size == len(data):
 
-    print(
-        f"Отримано: {data.decode('utf-8')} ({time.strftime('%Y-%m-%d %H:%M:%S')})")
-    client_socket.sendall(("Ви надіслали: "+data).encode('utf-8'))
-else:
-    print(f"Дані пошкоджено")
-    client_socket.sendall(("Дані пошкоджено: "+data).encode('utf-8'))
+        print(
+            f"Отримано: {data} ({time.strftime('%Y-%m-%d %H:%M:%S')})")
+        client_socket.sendall(("Ви надіслали: "+data).encode('utf-8'))
+    else:
+        print(f"Дані пошкоджено")
+        client_socket.sendall(("Дані пошкоджено: "+data).encode('utf-8'))
 
 
 client_socket.close()
