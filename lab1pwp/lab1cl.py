ab8f6b039155c81b96a1e3e661053c44c1c16562
maidennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn <srazihely@gmail.com>
2023-10-11 18:43:52 +0300

    task4 added sequential user login support

--- lab1pwp/lab1cl.py
+++ lab1pwp/lab1cl.py
@@ -1,17 +1,17 @@
 import socket
 
 host = '127.0.0.1'
-port = 52345
+port = 43287
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((host, port))
 while True:
     message = input("Введіть текст: ")
 
-    client_socket.sendall(message.encode('utf-8'))
-    client_socket.sendall(str(len(message)).encode('utf-8'))
+    client_socket.send(message.encode('utf-8'))
     if message == "stop":
         break
+    client_socket.send(str(len(message)).encode('utf-8'))
     data = client_socket.recv(1024).decode('utf-8')
     print(data)
 
--- lab1pwp/lab1server.py
+++ lab1pwp/lab1server.py
@@ -2,35 +2,33 @@ import socket
 import time
 
 host = '127.0.0.1'
-port = 52345
+port = 43287
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((host, port))
 
 server_socket.listen(1)
 print(f"Сервер: {host}:{port}")
-
-client_socket, client_address = server_socket.accept()
-print(f"{client_address} приєднався")
-
 while True:
-    data = client_socket.recv(1024).decode('utf-8')
-    size = int(client_socket.recv(1024).decode('utf-8'))
-    if not data:
-        break
-    if data == "stop":
-        break
-    # time.sleep(5)
+    client_socket, client_address = server_socket.accept()
+    print(f"{client_address} приєднався")
 
-    if size == len(data):
+    while True:
+        data = client_socket.recv(1024).decode('utf-8')
+        if data.lower() == "stop":
+            break
+        size = client_socket.recv(1024).decode('utf-8')
+        if not data:
+            break
+        # time.sleep(5)
 
-        print(
-            f"Отримано: {data} ({time.strftime('%Y-%m-%d %H:%M:%S')})")
-        client_socket.sendall(("Ви надіслали: "+data).encode('utf-8'))
-    else:
-        print(f"Дані пошкоджено")
-        client_socket.sendall(("Дані пошкоджено: "+data).encode('utf-8'))
+        if int(size) == int(len(data)):
+            print(
+                f"Отримано: {data} ({time.strftime('%Y-%m-%d %H:%M:%S')})")
+            client_socket.send(("Ви надіслали: "+data).encode('utf-8'))
+        else:
+            print(f"Дані пошкоджено")
+            client_socket.send(("Дані пошкоджено: "+data).encode('utf-8'))
 
-
-client_socket.close()
+    client_socket.close()
 server_socket.close()
