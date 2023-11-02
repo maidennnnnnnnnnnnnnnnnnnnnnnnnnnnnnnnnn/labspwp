2dbce725b382458591ffa6e56a6c6d81291bb45a
maidennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn <srazihely@gmail.com>
2023-11-03 01:11:25 +0200

    created feedback page, created database to store feedbacks and updated about page

--- selepiy_project/run.py
+++ selepiy_project/run.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'asdasd'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///feedbacks.db'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
@@ -14,9 +23,31 @@ def about():
 def portfolio():
     return render_template('portfolio.html')
 
-@app.route('/feedback')
+class Feedback(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    comment = db.Column(db.Text, nullable=False)
+
+class FeedbackForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    comment = TextAreaField('Comment', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/feedback', methods=['GET', 'POST'])
 def feedback():
-    return render_template('feedback.html')
+    form = FeedbackForm()
+
+    if form.validate_on_submit():
+        feedback = Feedback(name=form.name.data, comment=form.comment.data)
+        db.session.add(feedback)
+        db.session.commit()
+        flash('Feedback submitted successfully', 'success')
+        return redirect(url_for('feedback'))
+
+    feedbacks = Feedback.query.all()
+    return render_template('feedback.html', form=form, feedbacks=feedbacks)
+
+asd = Migrate(app, db)
 
 if __name__ == '__main__':
     app.run(debug=True)
