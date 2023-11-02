d94ce1e86e1fa3c52d1e19696d04d08ca4035472
maidennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn <srazihely@gmail.com>
2023-11-02 23:12:52 +0200

    created basic feedback page layout

--- selepiy_project/run.py
+++ selepiy_project/run.py
@@ -14,5 +14,9 @@ def about():
 def portfolio():
     return render_template('portfolio.html')
 
+@app.route('/feedback')
+def feedback():
+    return render_template('feedback.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
